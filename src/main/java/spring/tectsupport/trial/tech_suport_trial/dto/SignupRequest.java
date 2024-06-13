42bf3ff7e235c1f83f654f2686929352f437a256
Piriyalini Niththiyanantham <priyalini.18@itfac.mrt.ac.lk>
2024-06-13 14:59:18 +0530

    custom validations added to strong password and confirm password

--- src/main/java/spring/tectsupport/trial/tech_suport_trial/dto/SignupRequest.java
+++ src/main/java/spring/tectsupport/trial/tech_suport_trial/dto/SignupRequest.java
@@ -3,9 +3,16 @@ package spring.tectsupport.trial.tech_suport_trial.dto;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
+import spring.tectsupport.trial.tech_suport_trial.validation.PasswordMatching;
+import spring.tectsupport.trial.tech_suport_trial.validation.StrongPassword;
 
 import java.util.Set;
 
+@PasswordMatching(
+        password = "password",
+        confirmPassword = "confirmPassword",
+        message = "Password and Confirm Password must be matched!"
+)
 public class SignupRequest {
     @NotBlank
     @Size(min = 3, max = 20)
@@ -20,7 +27,10 @@ public class SignupRequest {
 
     @NotBlank
     @Size(min = 6, max = 40)
+    @StrongPassword
     private String password;
+
+    private String confirmPassword;
 
     public String getUsername() {
         return username;
@@ -45,6 +55,10 @@ public class SignupRequest {
     public void setPassword(String password) {
         this.password = password;
     }
+
+    public String getConfirmPassword(){return confirmPassword;}
+
+    public void setConfirmPassword(String confirmPassword){this.confirmPassword = confirmPassword;}
 
     public Set<String> getRole() {
         return this.role;
